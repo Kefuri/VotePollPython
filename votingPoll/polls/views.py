ab60c3eda37efdf7f8fdece26f3573accbb2672a
Haydon Houghton <haydonhoughton@outlook.com>
2020-05-29 11:22:12 +0100

    Add new views for question results and voting options

--- votingPoll/polls/views.py
+++ votingPoll/polls/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Sup dawg, this the index.")
+
+def detail(request, question_id):
+  response = "You're looking at question %s"
+
+  return HttpResponse(response %question_id)
+
+def results(request, question_id):
+  return HttpResponse("This is the result for question %s" %question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're about to vote on question %s" %question_id)
