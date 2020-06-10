a45bd68f481300520721dbd45c0272b00066fc26
Haydon Houghton <haydonhoughton@outlook.com>
2020-06-10 12:51:08 +0100

    Rework qet_queryset and remove unnecessary result method

--- votingPoll/polls/views.py
+++ votingPoll/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from django.views import generic
 from django.urls import reverse
 from django.http import HttpResponse, Http404, HttpResponseRedirect
@@ -10,7 +11,10 @@ class IndexView(generic.ListView):
   context_object_name = 'latest_question_list'
 
   def get_queryset(self):
-    return Question.objects.order_by('-pub_date')[:5]
+    
+    return Question.objects.filter(
+      pub_date__lte=timezone.now()
+    ).order_by('pub_date')[:5]
 
 class DetailView(generic.DetailView):
   model = Question
@@ -33,7 +37,3 @@ def vote(request, question_id):
     selected_choice.votes += 1
     selected_choice.save()
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-  def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
