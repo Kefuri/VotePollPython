d526345a7b592517028a729c565dec59b01c2c50
Haydon Houghton <haydonhoughton@outlook.com>
2020-06-11 12:27:57 +0100

    add tests for detail view, add static css for index

--- votingPoll/polls/admin.py
+++ votingPoll/polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInLine(admin.StackedInline):
+  model = Choice
+  extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  fieldsets = [
+    (None,  {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date']}),
+  ]
+  inlines = [ChoiceInLine]
+
+admin.site.register(Question, QuestionAdmin)
